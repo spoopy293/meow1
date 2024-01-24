20e0ff4ad7f47b7c8243c027d8d2acf1ba8abccb
meow <ashley-293@hotmail.com>
2024-01-24 13:39:59 -0700

    meowmeow

--- pages/admin.vue
+++ pages/admin.vue
@@ -42,5 +42,64 @@ const user = useSupabaseUser();
       <!-- Show user information -->
       <p v-if="user">{{ user.email }}</p>
     </section>
+    <h1>Employees</h1>
+    <!-- render the fetched data -->
+    <ul>
+      <li v-for="employee in eMPLOYEES" :key="employee.EMPLOYEE_ID">
+        <h3>Name: {{ employee.FIRST_NAME }} {{ employee.LAST_NAME }}</h3>
+        <p>Email: {{ employee.EMAIL }}</p>
+        <p>Phone: {{ employee.PHONE_NUMBER }}</p>
+      </li>
+    </ul>
+    <h1>Insert New Data</h1>
+    <section>
+      <label for="EMPLOYEE_ID">
+        Employee ID
+        <input type="EMPLOYEE_ID" v-model="EMPLOYEE_ID" />
+      </label><br>
+      <label for="FIRST_NAME">
+        First Name
+        <input type="FIRST_NAME" v-model="FIRST_NAME" />
+      </label><br>
+      <label for="LAST_NAME">
+        Last Name
+        <input type="LAST_NAME" v-model="LAST_NAME" />
+      </label><br>
+      <label for="EMAIL">
+        Email
+        <input type="EMAIL" v-model="EMAIL" />
+      </label><br>
+      <label for="PHONE_NUMBER">
+        Phone Number
+        <input type="PHONE_NUMBER" v-model="PHONE_NUMBER" />
+      </label><br>
+      <label for="HIRE_DATE">
+        Hire Date
+        <input type="HIRE_DATE" v-model="HIRE_DATE" />
+      </label><br>
+      <label for="JOB_ID">
+        Job ID
+        <input type="JOB_ID" v-model="JOB_ID" />
+      </label><br>
+      <label for="SALARY">
+        Salary
+        <input type="SALARY" v-model="SALARY" />
+      </label><br>
+      <label for="COMMISSION_PCT">
+        Commission Percentage
+        <input type="COMMISSION_PCT" v-model="COMMISSION_PCT" />
+      </label><br>
+      <label for="MANAGER_ID">
+        Manager ID:
+        <input type="MANAGER_ID" v-model="MANAGER_ID" />
+      </label><br>
+      <label for="DEPARTMENT_ID">
+        Department ID
+        <input type="DEPARTMENT_ID" v-model="DEPARTMENT_ID" />
+      </label><br>
+    </section>
+   <section>
+      <button @click="Insert">Insert Data</button>
+    </section>
   </main>
 </template>
